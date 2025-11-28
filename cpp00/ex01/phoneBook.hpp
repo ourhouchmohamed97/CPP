52bf27cce6c54cdad42584948c3dcfbdf457eb55
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-11-28 15:43:52 +0100

    feat: add contact class

--- cpp00/ex01/phoneBook.hpp
+++ cpp00/ex01/phoneBook.hpp
@@ -4,5 +4,39 @@
 
 #include <iostream>
 
+class PhoneBook {
+    public:
+        PhoneBook();
+    private:
+        Contact contacts[8];
+        int     nextContact;
+        int     countContact;
+};
+
+class Contact {
+    // fields
+    private:
+        std::string firstName;
+        std::string lastName;
+        std::string nickName;
+        std::string phoneNumber;
+        std::string darkestSecret;
+
+    // default construct
+    public:
+        Contact();
+    // setters
+    void    setFirstName(const std::string &value);
+    void    setLastName(const std::string &value);
+    void    setNickName(const std::string &value);
+    void    setPhoneNumber(const std::string &value);
+    void    setDarkestSecret(const std::string &value);
+    // getters
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    std::string getNickName() const;
+    std::string getPhoneNumber() const;
+    std::string getDarkestSecret() const;
+};
 
 #endif
