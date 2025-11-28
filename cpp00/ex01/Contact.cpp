75889c9c0fa0dcca42096df189f6b629083180ca
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-11-28 15:42:55 +0100

    feat: work on contact

---
+++ cpp00/ex01/Contact.cpp
@@ -0,0 +1,42 @@
+#include "PhoneBook.hpp"
+
+Contact::Contact() {
+    firstName = "";
+    lastName = "";
+    nickName = "";
+    phoneNumber = "";
+    darkestSecret = "";
+}
+
+void    Contact::setFirstName(const std::string &value) {
+    firstName = value;
+}
+void    Contact::setLastName(const std::string &value) {
+    lastName = value;
+}
+void    Contact::setNickName(const std::string &value) {
+    nickName = value;
+}
+void    Contact::setPhoneNumber(const std::string &value) {
+    phoneNumber = value;
+}
+void    Contact::setDarkestSecret(const std::string &value) {
+    darkestSecret = value;
+}
+
+
+std::string Contact::getFirstName() const {
+    return firstName;
+}
+std::string Contact::getLastName() const {
+    return lastName;
+}
+std::string Contact::getNickName() const {
+    return nickName;
+}
+std::string Contact::getPhoneNumber() const {
+    return phoneNumber;
+}
+std::string Contact::getDarkestSecret() const {
+    return darkestSecret;
+}
