3ad4a42102dd284fba82a2d1bfb880dad64962a4
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-02 11:51:30 +0100

    feat: fix 10 character limitation

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -21,6 +21,7 @@ void    add_contact(PhoneBook &phonebook) {
 
     std::cout << "Nick Name: ";
     std::getline(std::cin, input);
+
     if(std::cin.eof())  return;
     c.setNickName(input);
 
@@ -61,24 +62,34 @@ void    PhoneBook::displaySpecificContact(int index) const {
     std::cout << "Darkest Secret: " << c.getDarkestSecret() << std::endl;
 }
 
+void printField(const std::string &s) {
+    if (s.length() > 10)
+        std::cout << s.substr(0, 9) << ".";
+    else
+        std::cout << std::setw(10) << s;
+}
+
 void    PhoneBook::displayAllContacts() {
     if (countContact == 0) {
         std::cout << "PhoneBook is empty" << std::endl;
         return ;
     }
 
-    std::cout << "Index" << "|"
-        << "First Name" << "|"
-        << "Last Name" << "|"
-        << "Nick Name" << "|" << std::endl;
+    std::cout << std::setw(10) << "Index" << '|'
+              << std::setw(10) << "First Name" << '|'
+              << std::setw(10) << "Last Name" << '|'
+              << std::setw(10) << "Nickname" << std::endl;
     
-    for(int i = 0; i < countContact; i++) {
-        std::cout << i << "|";
-        std::cout << contacts[i].getFirstName();
+    for (int i = 0; i < countContact; i++) {
+        std::cout << std::setw(10) << i << "|";
+
+        printField(contacts[i].getFirstName());
         std::cout << "|";
-        std::cout << contacts[i].getLastName();
+
+        printField(contacts[i].getLastName());
         std::cout << "|";
-        std::cout << contacts[i].getNickName();
+
+        printField(contacts[i].getNickName());
         std::cout << std::endl;
     }
 }
@@ -95,11 +106,21 @@ void    search_contact(PhoneBook &phonebook) {
     std::string input;
     std::cout << "Enter the index: ";
     std::getline(std::cin, input);
+
+    if(std::cin.eof())
+        return ;
+
+    if (input.empty()) {
+        std::cout << "Invalid input: empty value" << std::endl;
+        return;
+    }
+
     if(!isNumber(input)) {
         std::cout << "Invalid input: not a number" << std::endl;
         return ;
     }
-    int index = std::stoi(input);
+
+    int index = atoi(input.c_str());
 
     phonebook.displaySpecificContact(index);
 }
