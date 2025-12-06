d02efa853cbd4cd4eafac1b1e0544e218dea12b9
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-06 12:46:05 +0100

    feat: zombie.cpp

---
+++ cpp01/ex01/Zombie.cpp
@@ -0,0 +1,12 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie() {}
+
+Zombie::~Zombie() {
+    std::cout << name << " is destroyed." << std::endl;
+}
+
+void Zombie::announce(void)
+{
+    std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
