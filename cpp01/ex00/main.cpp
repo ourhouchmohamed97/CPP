559a35e1dd3d1e3f124cf9bbc07dad133150a4d6
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-06 12:43:05 +0100

    feat: complete work

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -4,10 +4,16 @@ int main() {
 
     std::string input;
     std::getline(std::cin, input);
-    if(std::cin.eof())
-        return 1;
 
     Zombie zombie(input);
     zombie.announce();
-    return 0;    
+
+    Zombie* z = newZombie("Lurch");
+    z->announce();
+
+    randomChump("Bub");
+    
+    delete z;
+
+    return 0;
 }
