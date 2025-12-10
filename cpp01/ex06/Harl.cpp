00b3cc1b3feed5252479a19009a48e407cb48119
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-10 16:28:06 +0100

    feat: ex05 harl.cpp

---
+++ cpp01/ex06/Harl.cpp
@@ -0,0 +1,47 @@
+#include "Harl.hpp"
+
+void Harl::debug( void ) {
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info( void ) {
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning( void ) {
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+}
+
+void Harl::error( void ) {
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+int levelToInt( std::string level ) {
+    if(level == "DEBUG") return 0;
+    if(level == "INFO") return 1;
+    if(level == "WARNING") return 2;
+    if(level == "ERROR") return 3;
+    return -1;
+}
+
+void Harl::complain( std::string level ) {
+    int code = levelToInt(level);
+
+    switch (code)
+    {
+        case 0:
+            debug();
+        case 1:
+            info();
+        case 2:
+            warning();
+        case 3:
+            error();
+        default :
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    }
+}
