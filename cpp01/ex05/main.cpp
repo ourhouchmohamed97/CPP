8bf62810a4836fdc05067d98ef0289acd7ca9332
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-10 16:27:07 +0100

    feat: ex05 main

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+
+int main(int ac, char **av) {
+    if(ac != 2) {
+        std::cout << "Usage: ./Harl <LEVEL>" << std::endl;
+        return 1;
+    }
+    Harl h;
+    std::string level = av[1];
+    h.complain(av[1]);
+
+    return 0;
+}
