0f33f1387286ab4c593eb3e8a35a69a4f5f30a5e
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-11 12:49:55 +0100

    feat: modified main in ex02

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -1,6 +1,6 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB( const std::string& name ) : name(name), weapon(nullptr) {} // start unarmed
+HumanB::HumanB( const std::string& name ) : name(name), weapon(0) {} // start unarmed
 
 void HumanB::setWeapon( Weapon& newWeapon ) {
     weapon = &newWeapon;
--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -13,7 +13,6 @@ int main()
     }
     {
         Weapon club = Weapon("crude spiked club");
-
         HumanB jim("Jim");
         jim.setWeapon(club);
         jim.attack();
