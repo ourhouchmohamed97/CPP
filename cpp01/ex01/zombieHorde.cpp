203028211ce442140b0f01e7c771c3c0d52e3842
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-11 12:48:31 +0100

    feat: modified zombiehorde in ex01

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -1,10 +1,14 @@
 #include "Zombie.hpp"
 
-void    Zombie::setName(std::string newName) {
+void    Zombie::setName( std::string newName ) {
     name = newName;
 }
 
 Zombie* zombieHorde( int N, std::string name ) {
+    if (N <= 0) {
+        std::cerr << "Error: N must be > 0" << std::endl;
+        return 0;
+    }
     Zombie* zombieHorde = new Zombie[N];
 
     for(int i = 0; i < N; i++) {
