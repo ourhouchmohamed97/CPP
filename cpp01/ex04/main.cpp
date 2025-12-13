1ee23c5f6c1c13804ab888913a8e29bd707fb6e5
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-13 18:00:11 +0100

    feat: main ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -45,3 +45,7 @@ int main(int ac, char **av) {
     outfile << content;
     outfile.close();
 }
+
+// **npos** is a static member constant value with the greatest possible value for an element of type size_t.
+// As a return value, it is usually used to indicate no matches.
+// This constant is defined with a value of -1, which because size_t is an unsigned integral type, it is the largest possible representable value for this type.
