04dfe5de5714cce22dd40b16f7150f4b4565016c
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-13 17:59:46 +0100

    feat: remove the indice from the name

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -12,7 +12,7 @@ Zombie* zombieHorde( int N, std::string name ) {
     Zombie* zombieHorde = new Zombie[N];
 
     for(int i = 0; i < N; i++) {
-        zombieHorde[i].setName(name + "_" + std::to_string(i));
+        zombieHorde[i].setName(name);
     }
     return zombieHorde;
 }
