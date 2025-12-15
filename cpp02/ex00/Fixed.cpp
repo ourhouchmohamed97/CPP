447b51cc393b281afd09e1e1332f4044b3e64129
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-15 18:23:22 +0100

    feat: working in ex00, fixed.cpp

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,32 @@
+#include "Fixed.hpp"
+
+const int Fixed::fractional = 8;
+
+Fixed::Fixed() : value(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    if(this != &other)
+        this->value = other.value;
+    return *this;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return value;
+}
+
+void Fixed::setRawBits( int const raw ) {
+    value = raw;
+}
