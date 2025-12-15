0b72df85242e39b576ff78f614570c23e27be2f2
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-15 18:23:33 +0100

    feat: working in ex00, fixed.hpp

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -1,3 +1,19 @@
 #pragma once
 
 #include <iostream>
+
+
+class Fixed {
+    private:
+        int value;
+        static const int fractional;
+
+    public:
+        Fixed(); // default constructor
+        Fixed(const Fixed& other); // copy constructor
+        Fixed& operator=(const Fixed& other); // copy assignment
+        ~Fixed(); // destructor
+
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+};
