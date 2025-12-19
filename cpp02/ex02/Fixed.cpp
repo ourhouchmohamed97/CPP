96ce630a913a0bfd271e401eb04905c22fd4fbf6
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-19 11:19:34 +0100

    feat: complete the work

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -1,34 +1,26 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed() : value(0) {
-    std::cout << "Default constructor called" << std::endl;
-}
+Fixed::Fixed() : value(0) {}
 
 Fixed::Fixed(const int n) {
-    std::cout << "Int constructor called" << std::endl;
     value = n << fractional;
 }
 
 Fixed::Fixed(const float f) {
-    std::cout << "Float constructor called" << std::endl;
     value = roundf(f * (1 << fractional));
 }
 
 Fixed::Fixed(const Fixed& other) {
-    std::cout << "Copy constructor called" << std::endl;
     *this = other;
 }
 
 Fixed& Fixed::operator=(const Fixed& other) {
-    std::cout << "Copy assignment operator called" << std::endl;
     if (this != &other)
         value = other.value;
     return *this;
 }
 
-Fixed::~Fixed() {
-    std::cout << "Destructor called" << std::endl;
-}
+Fixed::~Fixed() {}
 
 float Fixed::toFloat( void ) const {
     return (float)value / (1 << fractional);
@@ -36,6 +28,99 @@ float Fixed::toFloat( void ) const {
 
 int Fixed::toInt( void ) const {
     return value >> fractional;
+}
+
+// comparison operators
+
+bool Fixed::operator<(const Fixed& other) const {
+    return this->value < other.value;
+}
+
+bool Fixed::operator<=(const Fixed& other) const {
+    return this->value <= other.value;
+}
+
+bool Fixed::operator>(const Fixed& other) const {
+    return this->value > other.value;
+}
+
+bool Fixed::operator>=(const Fixed& other) const {
+    return this->value >= other.value;
+}
+
+bool Fixed::operator==(const Fixed& other) const {
+    return this->value == other.value;
+}
+
+bool Fixed::operator!=(const Fixed& other) const {
+    return this->value != other.value;
+}
+
+// arithmetic operators
+
+Fixed Fixed::operator+(const Fixed& other) const{
+    Fixed result;
+    result.value = this->value + other.value;
+    return result;
+}
+
+Fixed Fixed::operator-(const Fixed& other) const{
+    Fixed result;
+    result.value = this->value - other.value;
+    return result;
+}
+
+Fixed Fixed::operator*(const Fixed& other) const{
+    Fixed result;
+    result.value = (this->value * other.value) >> fractional;
+    return result;
+}
+
+Fixed Fixed::operator/(const Fixed& other) const{
+    Fixed result;
+    result.value = (this->value << fractional) / other.value;
+    return result;
+}
+
+// Increment / Decrement operators
+
+Fixed& Fixed::operator++() {
+    this->value++;
+    return *this;
+}
+
+Fixed Fixed::operator++(int) {
+    Fixed tmp(*this);
+    this->value++;
+    return tmp;
+}
+
+Fixed& Fixed::operator--() {
+    this->value--;
+    return *this;
+}
+
+Fixed Fixed::operator--(int) {
+    Fixed tmp(*this);
+    this->value--;
+    return tmp;
+}
+
+
+// Min and Max functions
+Fixed& Fixed::min(Fixed& fixed1, Fixed& fixed2) {
+    return (fixed1.value < fixed2.value) ? fixed1 : fixed2;
+}
+
+const Fixed& Fixed::min(const Fixed& fixed1, const Fixed& fixed2) {
+    return (fixed1.value < fixed2.value) ? fixed1 : fixed2;
+}
+
+Fixed& Fixed::max(Fixed& fixed1, Fixed& fixed2) {
+    return (fixed1.value > fixed2.value) ? fixed1 : fixed2;
+}
+const Fixed& Fixed::max(const Fixed& fixed1, const Fixed& fixed2) {
+    return (fixed1.value > fixed2.value) ? fixed1 : fixed2;
 }
 
 std::ostream& operator<<(std::ostream& os, const Fixed& fixed)
