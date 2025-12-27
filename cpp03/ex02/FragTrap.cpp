ad46fbb8f9ce9df0254215a29410e4efb7a9c87e
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-27 15:46:22 +0100

    feat: fragtrap.cpp

---
+++ cpp03/ex02/FragTrap.cpp
@@ -0,0 +1,38 @@
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() : ClapTrap() {
+    _hitPoints = 100;
+    _energyPoints = 100;
+    _attackDamage = 30;
+    std::cout << "FragTrap default constructor called" << std::endl;
+}
+
+FragTrap::FragTrap( std::string name ) : ClapTrap(name)
+{
+    _hitPoints = 100;
+    _energyPoints = 100;
+    _attackDamage = 30;
+    std::cout << "FragTrap parameterized constructor called for " << _name << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other)
+{
+    std::cout << "FragTrap copy constructor called" << std::endl;
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& other)
+{
+    if(this != &other)
+        ClapTrap::operator=(other);
+    std::cout << "FragTrap copy assignment called" << std::endl;
+    return *this;
+}
+
+FragTrap::~FragTrap() {
+    std::cout << "FragTrap destructor called for " << _name << std::endl;
+}
+
+
+void FragTrap::highFivesGuys( void ) {
+    std::cout << "FragTrap " << _name << " requests a positive high five!" << std::endl;
+}
