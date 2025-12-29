77beaf3d1d58eb536861f8ce23a12a93c669e72e
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-29 16:29:15 +0100

    Add default contructor

--- cpp03/ex03/DiamondTrap.hpp
+++ cpp03/ex03/DiamondTrap.hpp
@@ -7,6 +7,7 @@ class DiamondTrap : public ScavTrap, public FragTrap {
     private:
         std::string _name;
     public:
+        DiamondTrap();
         DiamondTrap( std::string name );
         DiamondTrap( const DiamondTrap& other );
         DiamondTrap& operator=( const DiamondTrap& other );
