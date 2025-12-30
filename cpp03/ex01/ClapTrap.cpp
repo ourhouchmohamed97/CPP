127eeee3ede862a12b5b7fdad64b9b6f2dbd1ddf
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-30 11:01:25 +0100

    Change ClapTrap ex01

--- cpp03/ex01/ClapTrap.cpp
+++ cpp03/ex01/ClapTrap.cpp
@@ -1,12 +1,19 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() : _name("default"),  _hitPoints(10), _energyPoints(10), _attackDamage(0) 
+ClapTrap::ClapTrap()
+    : _name("default"),
+      _hitPoints(10),
+      _energyPoints(10),
+      _attackDamage(0) 
 {
     std::cout << "ClapTrap default constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name) 
-    : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) 
+    : _name(name),
+      _hitPoints(10),
+      _energyPoints(10),
+      _attackDamage(0) 
 {
     std::cout << "ClapTrap parameterized constructor called for " << _name << std::endl;
 }
