f0cccea978d356757bef8d21d321aba5f6855171
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-31 13:24:35 +0100

    cat.cpp

---
+++ cpp04/ex00/Cat.cpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Animal.hpp"
+
+class Cat : public Animal {
+    private:
+
+    public:
+        Cat();
+        Cat( const Cat& other );
+        Cat& operator=(const Cat& other);
+        ~Cat();
+};
