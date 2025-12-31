66bb8b15f3a1b0653ae1ed83a378cf5db05a096a
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-31 13:24:09 +0100

    Animal.cpp

---
+++ cpp04/ex00/Animal.cpp
@@ -0,0 +1,26 @@
+#include "Animal.hpp"
+
+Animal::Animal() : _type("default") {
+    std::cout << "Animal created! [default constructor]" << std::endl;
+}
+
+Animal::Animal( std::string const &type ) : _type(type) {
+    std::cout << "Animal created! [parameterized constructor]" << std::endl;
+}
+
+Animal::Animal( const Animal& other ) : _type(other._type) {
+    std::cout << "Animal created! [copy constructor]" << std::endl;
+}
+
+Animal& Animal::operator=(const Animal& other ) {
+    std::cout << "Animal assigned! [copy assignment]" << std::endl;
+    if(this != &other)
+        this->_type = other._type;
+    return *this;
+}
+
+Animal::~Animal() {
+    std::cout << "Animal killed! [destructor]" << std::endl;
+}
+
+        void makeSound();
