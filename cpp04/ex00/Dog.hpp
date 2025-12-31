a79fa205a8e28343e3243fa43b05a85ae725f2d9
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-31 13:25:07 +0100

    Dog.hpp

---
+++ cpp04/ex00/Dog.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Animal.hpp"
+
+class Dog : public Animal {
+    private:
+
+    public:
+        Dog();
+        Dog( const Dog& other );
+        Dog& operator=(const Dog& other);
+        ~Dog();
+};
