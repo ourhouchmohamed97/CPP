06c93e2083de7891dcca9f5c605980d4056a327b
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-31 20:58:08 +0100

    WrongAnimal.cpp

---
+++ cpp04/ex01/WrongAnimal.cpp
@@ -0,0 +1,28 @@
+#include "WrongAnimal.hpp"
+
+WrongAnimal::WrongAnimal() : _type("default") {
+    std::cout << "WrongAnimal created! [default constructor]" << std::endl;
+}
+
+WrongAnimal::WrongAnimal( std::string const &type ) : _type(type) {
+    std::cout << "WrongAnimal created! [parameterized constructor]" << std::endl;
+}
+
+WrongAnimal::WrongAnimal( const WrongAnimal& other ) : _type(other._type) {
+    std::cout << "WrongAnimal copied! [copy constructor]" << std::endl;
+}
+
+WrongAnimal& WrongAnimal::operator=(const WrongAnimal& other ) {
+    std::cout << "WrongAnimal assigned! [copy assignment]" << std::endl;
+    if(this != &other)
+        this->_type = other._type;
+    return *this;
+}
+
+WrongAnimal::~WrongAnimal() {
+    std::cout << "WrongAnimal killed! [destructor]" << std::endl;
+}
+
+void WrongAnimal::makeSound() const {
+    std::cout << "* WrongAnimal makes a generic sound *" << std::endl;
+}
