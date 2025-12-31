5444112e736d3a0133a1c1cc5956ad031b1eaa77
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-31 19:19:26 +0100

    Cat.cpp

--- cpp04/ex00/Cat.cpp
+++ cpp04/ex00/Cat.cpp
@@ -1,13 +1,24 @@
-#pragma once
+#include "Cat.hpp"
 
-#include "Animal.hpp"
+Cat::Cat() : Animal("Cat") {
+    std::cout << "Cat created! [default constructor]" << std::endl;
+}
 
-class Cat : public Animal {
-    private:
+Cat::Cat( const Cat& other ) : Animal(other) {
+    std::cout << "Cat created! [copy constructor]" << std::endl;
+}
 
-    public:
-        Cat();
-        Cat( const Cat& other );
-        Cat& operator=(const Cat& other);
-        ~Cat();
-};
+Cat& Cat::operator=(const Cat& other) {
+    std::cout << "Cat assigned! [copy assignment]" << std::endl;
+    if(this != &other)
+        Animal::operator=(other);
+    return *this;
+}
+
+Cat::~Cat() {
+    std::cout << "Cat killed! [destructor]" << std::endl;
+}
+
+void Cat::makeSound() const {
+    std::cout << "Meow 🐱" << std::endl;
+}
