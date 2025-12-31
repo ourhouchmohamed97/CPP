8e2ad278b093e29c433e82a92cfa655b30320763
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-31 19:21:39 +0100

    WrongCat.cpp

---
+++ cpp04/ex00/WrongCat.cpp
@@ -0,0 +1,22 @@
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat() {
+    _type = "WrongCat";
+    std::cout << "WrongCat created!" << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat& other) : WrongAnimal(other) {}
+
+WrongCat& WrongCat::operator=(const WrongCat& other) {
+    if (this != &other)
+        WrongAnimal::operator=(other);
+    return *this;
+}
+
+WrongCat::~WrongCat() {
+    std::cout << "WrongCat destroyed!" << std::endl;
+}
+
+void WrongCat::makeSound() const {
+    std::cout << "Meow! (wrong)" << std::endl;
+}
