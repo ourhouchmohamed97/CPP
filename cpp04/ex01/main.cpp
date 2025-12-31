1e97852a67928d79a3316d02c505695f4d56b7c8
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-31 20:59:04 +0100

    main.cpp

---
+++ cpp04/ex01/main.cpp
@@ -0,0 +1,38 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+void check(){
+    system("leaks -q Animals");
+}
+
+int main()
+{
+    atexit(check);
+    std::cout << "\n===== SUBJECT TEST =====\n" << std::endl;
+    
+    const Animal* i = new Cat();
+    const Animal* j = new Dog();
+    delete j; //should not create a leak 
+    delete i;
+
+    std::cout << "\n===== MY TEST =====\n" << std::endl;
+
+    const int size = 10;
+    Animal* animals[size];
+
+    // Fill half with Dogs, half with Cats
+    for (int i = 0; i < size / 2; i++)
+        animals[i] = new Dog();
+
+    for (int i = size / 2; i < size; i++)
+        animals[i] = new Cat();
+
+    std::cout << "\n===== DESTRUCTION =====\n" << std::endl;
+
+    // Delete as Animals
+    for (int i = 0; i < size; i++)
+        delete animals[i];
+
+    return 0;
+}
