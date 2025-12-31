f66aafb09ab8f0825ee3dc37854cb096af0bdcc2
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-31 19:22:08 +0100

    Finish ex00

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -1,5 +1,25 @@
 #include "Animal.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
 
 int main() {
-    std::cout << "hello!";
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound();
+    j->makeSound();
+    meta->makeSound();
+
+    std::cout << "---------- Wrong test ----------" << std::endl;
+    const WrongAnimal* wc = new WrongCat();
+
+    wc->makeSound();
+
+    delete wc;
+    return 0;
 }
