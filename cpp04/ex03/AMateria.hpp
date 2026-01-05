89130d10eb8d614de01156d202089928ded1e65a
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-01-05 15:05:49 +0100

    AMateria.hpp

--- cpp04/ex03/AMateria.hpp
+++ cpp04/ex03/AMateria.hpp
@@ -2,16 +2,20 @@
 
 #include <iostream>
 #include "ICharacter.hpp"
+#include "AMateria.hpp"
 
+class ICharacter;
 class AMateria
 {
     protected:
         std::string _type;
+
     public:
-    AMateria(std::string const & type);
+    AMateria( std::string const & type );
+    virtual ~AMateria();
+    
     std::string const & getType() const;
     virtual AMateria* clone() const = 0;
-    virtual void use(ICharacter& target) = 0;
-    virtual ~AMateria();
+    virtual void use( ICharacter& target );
 };
 
