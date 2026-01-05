719008da4464ac819e7ed9953156b90a96f33800
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-01-05 15:09:15 +0100

    IMateriaSource.cpp

--- cpp04/ex03/IMateriaSource.hpp
+++ cpp04/ex03/IMateriaSource.hpp
@@ -3,11 +3,10 @@
 #include <iostream>
 #include "AMateria.hpp"
 
-
 class IMateriaSource
 {
     public:
         virtual ~IMateriaSource() {}
-        virtual void learnMateria(AMateria*) = 0;
-        virtual AMateria* createMateria(std::string const & type) = 0;
+        virtual void learnMateria( AMateria* ) = 0;
+        virtual AMateria* createMateria( std::string const & type ) = 0;
 };
