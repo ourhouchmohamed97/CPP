1dddf1ebfe51a0de83c043a19445969d9fee95c7
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-01-16 21:14:41 +0100

    Bureaucrat.hpp

---
+++ cpp05/ex01/Bureaucrat.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Form.hpp"
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat {
+    private:
+        const std::string _name;
+        int _grade;
+
+    public:
+        // OCF
+        Bureaucrat();
+        Bureaucrat( const std::string& name, int grade );
+        Bureaucrat( const Bureaucrat& other );
+        Bureaucrat& operator=( const Bureaucrat& other );
+        ~Bureaucrat();
+
+        // Getters
+        const std::string& getName() const;
+        int getGrade() const;
+
+        // Grade modification
+        void incrementGrade();
+        void decrementGrade();
+
+        void signForm(Form& form);
+        
+        // Exceptions
+        class GradeTooHighException : public std::exception {
+            public:
+                const char* what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception {
+            public:
+                const char* what() const throw();
+        };
+};
+
+std::ostream& operator<<( std::ostream& os, const Bureaucrat& bureaucrat );
