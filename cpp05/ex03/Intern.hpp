c1ab0de518a5e4c32ca5edd6cae3a5e7fe3410e7
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-01-20 12:31:31 +0100

    Intern.hpp

---
+++ cpp05/ex03/Intern.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "AForm.hpp"
+
+class Intern {
+    public :
+        Intern();
+        Intern( const Intern& other);
+        Intern& operator=( const Intern& other );
+        ~Intern();
+
+        AForm* makeForm(const std::string& name, const std::string& target );
+};
