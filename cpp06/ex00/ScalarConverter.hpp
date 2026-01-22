19364d36d7eb3541d0c0df55297be753ed5bbae3
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-01-22 13:05:52 +0100

    ScalarConverter.cpp

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <string>
+#include <iostream>
+#include <cstdlib>   // strtod
+#include <climits>   // INT_MIN, INT_MAX
+#include <cctype>    // isprint
+#include <cmath>     // isnan, isinf
 
 class ScalarConverter {
     private:
