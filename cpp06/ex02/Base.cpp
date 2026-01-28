3185f986b79fe5c2d24ab88239cfb4813bbd098a
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-01-28 16:12:57 +0100

    Base.cpp

--- cpp06/ex02/Base.cpp
+++ cpp06/ex02/Base.cpp
@@ -12,26 +12,29 @@ Base * generate(void) {
 }
 
 void identify(Base* p) {
-    dynamic_cast<A *>(p);
-    dynamic_cast<B *>(p);
-    dynamic_cast<C *>(p);
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
 }
 
 void identify(Base& p) {
     try {
-        A& a = dynamic_cast<A &>(p);
+        (void)dynamic_cast<A &>(p);
         std::cout << "A" << std::endl;
         return ;
     } catch(...) {}
 
     try {
-        B& b = dynamic_cast<B &>(p);
+        (void)dynamic_cast<B &>(p);
         std::cout << "B" << std::endl;
         return ;
     } catch(...) {}
 
     try {
-        C& c = dynamic_cast<C &>(p);
+        (void)dynamic_cast<C &>(p);
         std::cout << "C" << std::endl;
         return ;
     } catch(...) {}
