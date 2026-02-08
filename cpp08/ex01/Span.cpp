41391980b400ad3534ebfd30ef76ef99230f0578
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-02-08 14:06:43 +0100

    Implementation

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -1,5 +1,6 @@
 #include "Span.hpp"
 #include <exception>
+#include <algorithm>
 
 Span::Span( unsigned int N ) : N(N) {}
 
@@ -13,11 +14,35 @@ Span& Span::operator=( const Span& other ) {
 
 Span::~Span() {}
 
-void Span::addNumber() {
-    if ()
-        throw std::exception;
+void Span::addNumber( int value ) {
+    if (integers.size() >= N)
+        throw std::exception();
+    integers.push_back(value);
 }
 
-void Span::shortestSpan() {}
+int Span::shortestSpan() {
+    if (integers.size() < 2)
+        throw std::exception();
 
-void Span::longestSpan() {}
+    std::vector<int> tmp(integers);
+    std::sort(tmp.begin(), tmp.end());
+
+    int shortest = tmp[1] - tmp[0];
+
+    for (size_t i = 1; i < integers.size() - 1; i++) {
+        int diff = tmp[i + 1] - tmp[i];
+        if (diff < shortest)
+            shortest = diff;
+    }
+    return shortest;
+}
+
+int Span::longestSpan() {
+    if (integers.size() < 2)
+        throw std::exception();
+    
+    int min = *std::min_element(integers.begin(), integers.end());
+    int max = *std::max_element(integers.begin(), integers.end());
+
+    return max - min;
+}
