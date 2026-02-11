c81f9667733efe1315e426e1a5b17c8bc12ae821
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-02-11 11:53:39 +0100

    MutantStack.hpp

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,11 +1,25 @@
 #pragma once
 
-class MutantStack {
-    private:
+#include <stack>
 
+template <typename T>
+class MutantStack : public std::stack<T> {
     public:
-        MutantStack();
-        MutantStack( const MutantStack& other );
-        MutantStack& operator=( const MutantStack& other );
-        ~MutantStack();
+        typedef typename std::stack<T>::container_type::iterator iterator;
+
+        MutantStack() {}
+        MutantStack( const MutantStack& other ) : std::stack<T>( other ) {}
+        MutantStack& operator=( const MutantStack& other ) {
+            std::stack<T>::operator=(other);
+            return *this;
+        }
+        ~MutantStack() {}
+
+        iterator begin() {
+            return this->c.begin();
+        }
+
+        iterator end() {
+            return this->c.end();
+        }
 };
