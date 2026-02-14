15f8a5f0e6482fd57c66eeecbe21b27c58bf61b2
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-02-14 13:19:37 +0100

    Main

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,26 +1,51 @@
 #include "easyfind.hpp"
 #include <iostream>
 #include <vector>
+#include <list>
 
 int main() {
 
-    std::vector<int> arr;
+    // Vector conatiner test
+    std::vector<int> vect;
 
-    arr.push_back(10);
-    arr.push_back(20);
-    arr.push_back(30);
-    arr.push_back(40);
-    arr.push_back(50);
+    vect.push_back(10);
+    vect.push_back(20);
+    vect.push_back(30);
+    vect.push_back(40);
+    vect.push_back(50);
 
-    int value_to_find = 30;
+    int value_to_find;
+
+    std::cout << "Vector test:" << std::endl;
     try {
-        easyfind(arr, value_to_find);
-        std::cout << "value found!" << std::endl;
+        value_to_find = 30;
+        easyfind(vect, value_to_find);
+        std::cout << "value founded: " << value_to_find << std::endl;
         value_to_find = 60;
-        easyfind(arr, value_to_find);
+        easyfind(vect, value_to_find);
     }
     catch (std::exception  &e) {
         std::cout << e.what() << std::endl;
     }
+
+    // List container test
+    std::list<int> list;
+
+    list.push_back(10);
+    list.push_back(20);
+    list.push_back(30);
+
+    std::cout << "List test:" << std::endl;
+    try {
+        value_to_find = 20;
+        easyfind(list, value_to_find);
+        std::cout << "value founded: " << value_to_find << std::endl;
+        value_to_find = 60;
+        easyfind(vect, value_to_find);
+    }
+    catch(std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
     return 0;
 }
