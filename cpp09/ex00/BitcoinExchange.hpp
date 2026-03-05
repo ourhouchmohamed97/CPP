2c5c8c14d9a9a8f3748d35f3c5a15d5b871af89f
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-03-05 15:11:24 +0000

    add header with class and function prototypes

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -2,14 +2,22 @@
 
 #include <map>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 class BitcoinExchange {
     private:
         std::map<std::string, double> _database;
+
     public:
         BitcoinExchange();
         ~BitcoinExchange();
 
         void loadDB( const std::string& DB );
         void processInput( const std::string& input );
+
+        bool isValidDate( const std::string& date ) const;
+        bool isValidValue( const std::string& value ) const;
+        double getExchangeRate( const std::string& date ) const;
 };
