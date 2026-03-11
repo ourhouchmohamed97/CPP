0d8644c536e7e4d805729b76437a233ca25103ec
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-03-11 12:15:40 +0000

    RPN.cpp

---
+++ cpp09/ex01/RPN.cpp
@@ -0,0 +1,68 @@
+#include "RPN.hpp"
+#include <stdexcept>
+#include <sstream>
+
+RPN::RPN() {}
+
+RPN::RPN( const RPN &other ) {
+    *this = other;
+}
+
+RPN& RPN::operator=( const RPN &other ) {
+    if (this != &other) {}
+    return *this;
+}
+
+RPN::~RPN() {}
+
+bool RPN::valid( const std::string& input) {
+    for (size_t i = 0; i < input.size(); i++) {
+        if (!isdigit(input[i]) &&
+               input[i] != ' ' &&
+               input[i] != '+' &&
+               input[i] != '-' &&
+               input[i] != '*' &&
+               input[i] != '/')
+            return false;
+    }
+    return true;
+}
+
+
+int RPN::ExpressionEvaluator(const std::string &expression)
+{
+    for (size_t i = 0; i < expression.size(); i++) {
+        if (expression[i] == ' ')
+            continue;
+
+        if (isdigit(expression[i]))
+            _stack.push(expression[i] - '0');
+        else {
+            if (_stack.size() < 2)
+                throw std::runtime_error("Error");
+
+            int b = _stack.top(); _stack.pop();
+            int a = _stack.top(); _stack.pop();
+
+            if (expression[i] == '+')
+                _stack.push(a + b);
+            else if (expression[i] == '-')
+                _stack.push(a - b);
+            else if (expression[i] == '*')
+                _stack.push(a * b);
+            else if (expression[i] == '/')
+            {
+                if (b == 0)
+                    throw std::runtime_error("Error");
+                _stack.push(a / b);
+            }
+            else
+                throw std::runtime_error("Error"); // unknown character
+        }
+    }
+
+    if (_stack.size() != 1)
+        throw std::runtime_error("Error");
+
+    return _stack.top();
+}
