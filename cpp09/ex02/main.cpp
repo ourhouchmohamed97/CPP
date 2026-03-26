ce04d481880eba52d80d665d8b5d2dfe2d273038
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-03-26 18:47:14 +0100

    Main

---
+++ cpp09/ex02/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ctime>
+#include "PmergeMe.hpp"
+
+int main(int ac, char **av) {
+    if (ac < 2) {
+        std::cout << "Error" << std::endl;
+        return 1;
+    }
+
+    try {
+        PmergeMe p;
+        p.parseInput(ac, av);
+        p.sort();
+        p.display();
+    } catch(std::exception& e) {
+        std::cout << "Error" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
